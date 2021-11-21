1d285a820cfccd8dd08d36d5e42e57864474bbc9
Tej Prakash Prasad <8806463+prakashdrupal@users.noreply.github.com>
2021-11-21 15:25:43 +0530

    update

--- src/screens/About.js
+++ src/screens/About.js
@@ -7,46 +7,39 @@ function About() {
       <div className="App-text about">
         <div className="about-text__title">
           Hello, I'm{" "}
-          <span className="about-text__title-big">Pathetic Geek</span>.
+          <span className="about-text__title-big">Tej Prakash Prasad</span>.
         </div>
         <div className="about-text__title">
           I'm looking for <span className="about-text__title-big">Cool</span>{" "}
           works.
         </div>
         <div className="about-text__body">
-          I'm a self-taught full stack web & mobile developer who is always
+          I'm a self-taught full stack developer who is always
           learning new things.
           <br />
           <br />I am fluent in HTML5/CSS3, JavaScript, Node.js, React, Redux,
-          Next, React Native, Firebase, MongoDB, Puppeteer.
+          NextJs, Drupal, WordPress Firebase.
         </div>
         <div className="about-social">
           <div className="about-social__title">You can find me here</div>
           <div className="about-social__links">
             <a
               className="about-social__link"
-              href="https://instagram.com/patheticGeek"
+              href="https://prakashdrupal.wordpress.com/"
               rel="noopener noreferrer"
               target="_blank">
-              instagram
+              Blog
             </a>
             <a
               className="about-social__link"
-              href="https://github.com/patheticGeek"
+              href="https://github.com/prakashdrupal"
               rel="noopener noreferrer"
               target="_blank">
               github
             </a>
             <a
               className="about-social__link"
-              href="https://codesandbox.io/u/patheticGeek"
-              rel="noopener noreferrer"
-              target="_blank">
-              codesandbox
-            </a>
-            <a
-              className="about-social__link"
-              href="mailto:geekpathetic@gmail.com"
+              href="mailto:prasadejprakash172@gmail.com"
               rel="noopener noreferrer"
               target="_blank">
               email
